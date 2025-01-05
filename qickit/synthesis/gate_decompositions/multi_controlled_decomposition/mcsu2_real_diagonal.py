6a112f58272423dcd7508717c255e042980e752d
A.C.E07 <a.c.e8009aiden@gmail.com>
2025-01-05 20:41:44 +0800

    Update mcsu2_real_diagonal.py

--- qickit/synthesis/gate_decompositions/multi_controlled_decomposition/mcsu2_real_diagonal.py
+++ qickit/synthesis/gate_decompositions/multi_controlled_decomposition/mcsu2_real_diagonal.py
@@ -235,7 +235,6 @@ def mcsu2_real_diagonal_decomposition(
     circuit.add(s_gate, [target])
 
     mcx_2 = mcx_vchain_decomposition(num_control_qubits=k_2, output_framework=output_framework)
-    mcx_2.horizontal_reverse()
     circuit.add(mcx_2, controls[k_1:] + [target] + controls[k_1 - k_2 + 2 : k_1])
     circuit.add(s_gate_adjoint, [target])
 
